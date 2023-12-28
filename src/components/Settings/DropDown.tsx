9d06ed27cf9aa9fcfe1a6d6a997a974b01f06b7d
Ashhab <96653465+ahmedalashhab@users.noreply.github.com>
2023-12-28 23:20:07 +0300

    Update style of DropDown component
    Adjusted the styling attributes of the "Menu.Items" element in the DropDown component. Changes included making the border bold, rounding the corners, and changing the shadow effects.

--- src/components/Settings/DropDown.tsx
+++ src/components/Settings/DropDown.tsx
@@ -42,7 +42,10 @@ export const DropDown = ({ difficulty, setDifficulty }: DropDownProps) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items
+          className="absolute right-0 z-10 mt-2 w-56 origin-top-right bg-white shadow-mainCard
+        border-[3px] rounded-[20px] ring-1 ring-black ring-opacity-5 focus:outline-none font-bold"
+        >
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
