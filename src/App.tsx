172c0e04c6b46b3b1b4ab7566f51f8bbd99fa69a
Ashhab <96653465+ahmedalashhab@users.noreply.github.com>
2023-12-29 21:42:22 +0300

    Lower default game difficulty level
    The default difficulty level for the game has been reduced from 6 to 2. This change is intended to make the game more accessible and enjoyable for beginners. Users can still increase the difficulty level as per their preference later.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { PlayerVsPlayer } from "./components/PlayerVsPlayer/Player-Vs-Player";
 import { Settings } from "./components/Settings/Settings";
 
 function App() {
-  const [difficulty, setDifficulty] = useState<number>(6);
+  const [difficulty, setDifficulty] = useState<number>(2);
 
   //on boot, the difficulty is set to the value stored in the local storage
   React.useEffect(() => {
