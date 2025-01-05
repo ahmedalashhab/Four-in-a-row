b3f4d0048fe7bce0667fc86e82a3e6c7675f59e9
Ashhab <96653465+ahmedalashhab@users.noreply.github.com>
2025-01-05 17:51:43 +0300

    Refactor Firebase configuration to use environment variables and update .gitignore
    - Updated firebaseConfig in index.ts to utilize environment variables for sensitive information, enhancing security.
    - Added .env files to .gitignore to prevent accidental commits of sensitive configuration data.

--- src/firebase/config/index.ts
+++ src/firebase/config/index.ts
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { get, getDatabase, onValue, ref } from "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCx7iNOFlLtLweWTWPeWLlwhTv28OgLdLk",
-  authDomain: "connect-4-6f744.firebaseapp.com",
-  databaseURL:
-    "https://connect-4-6f744-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "connect-4-6f744",
-  storageBucket: "connect-4-6f744.appspot.com",
-  messagingSenderId: "17855025010",
-  appId: "1:17855025010:web:69c05efdc20fbdeb3a57eb",
-  measurementId: "G-KMJW9VYERN",
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 console.log("Initializing Firebase...");
